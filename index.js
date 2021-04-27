134ec81e2ee3f44d1116591fb5713d7d84472835
DalitsoKasonde <dkasonde11@gmail.com>
2021-04-27 11:38:11 +0200

    drum kit

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons ; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got Clicked!");
+
+    console.log(this);
+
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+
 });
 
 }
