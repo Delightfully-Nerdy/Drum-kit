27a8f5d652dc9481267e71ccaae1ea022a6bfb7e
DalitsoKasonde <dkasonde11@gmail.com>
2021-04-28 11:02:27 +0200

    fix play audio

--- index.js
+++ index.js
@@ -6,48 +6,48 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
     switch (buttonInnerHTML) {
       case "w":
-        var tom1 = new audio("sounds/tom-1.mp3");
+        var tom1 = new Audio("sounds/tom-1.mp3");
         tom1.play();
-
         break;
 
       case "a":
-        var tom2 = new audio("sounds/tom-2.mp3");
+        var tom2 = new Audio("sounds/tom-2.mp3");
         tom2.play();
-
         break;
 
       case "s":
-        var tom3 = new audio("sounds/tom-3.mp3");
+        var tom3 = new Audio("sounds/tom-3.mp3");
         tom3.play();
-
         break;
 
       case "d":
-        var tom4 = new audio("sounds/tom-4.mp3");
+        var tom4 = new Audio("sounds/tom-4.mp3");
         tom4.play();
-
         break;
 
       case "j":
-        var crash = new audio("sounds/crash.mp3");
-        crash.play();
-
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
         break;
 
       case "k":
-        var kick = new audio("sounds/kick-bass.mp3");
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+      case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
         kick.play();
 
         break;
 
-      case "l":
-        var snare = new audio("sounds/snare.mp3");
-        snare.play();
+      default: console.log(buttonInnerHTML);
 
-        break;
-      default:
-        break;
     }
+
+    addEventListener("keypress", function() {
+      alert("Key was pressed!");
+    })
+
   });
 }
